72283cf8633ba9b6bb1da3d09130ff146bb064eb
Wellington B. de Carvalho <9698691+tombrito@users.noreply.github.com>
2025-12-13 21:20:15 +0000

    feat: backgroundThrottling: false - isso ajuda a evitar que o Electron pause o WebGL quando a janela perde foco.

--- main.js
+++ main.js
@@ -47,7 +47,8 @@ function createWindow() {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
-      nodeIntegration: false
+      nodeIntegration: false,
+      backgroundThrottling: false
     }
   });
 
